94f74aaf3c2704a99cdc371a6a94a5a29dc3c182
jansfelt <jansfelt@student.chalmers.se>
2021-04-21 09:51:52 +0200

    update

--- project/database/readCSV.js
+++ project/database/readCSV.js
@@ -5,7 +5,7 @@ let stream = fs.createReadStream("project/database/portfolio.csv");
 let csvData = [];
 let csvStream = fastcsv
   .parse()
-  .on("data", function(data) {
+  .on("data", delimiter =";", function(data, delimiter) {
     csvData.push(data);
   })
   .on("end", function() {
@@ -16,33 +16,36 @@ let csvStream = fastcsv
     // save csvData
   });
 
+
 stream.pipe(csvStream);
 
 
 const Pool = require("pg").Pool;
+//const { delimiter } = require("node:path");
 
 // remove the first line: header
-
-for (i=1;i<=8;i++){
+for (i=0;i<7;i++){
   csvData.shift();
 }
+
+
 // create a new connection pool to the database
 const pool = new Pool({
   host: "localhost",
   user: "postgres",
   database: "eniacdb",
   password: "postgres",
-  port: 5432
+  port: 5432  
 });
 
 const query =
-  "INSERT INTO Stocks (name, price, country, procent) VALUES ($1, $2, $3, $4)";
+  "INSERT INTO Labels (name, weight) VALUES ($1, $2)";
 
 pool.connect((err, client, done) => {
   if (err) throw err;
   try {
     csvData.forEach(row => {
-      client.query(query, [row[0], 20, 'SE', 0.17] , (err, res) => {
+      client.query(query, row, (err, res) => {
         if (err) {
           console.log(err.stack);
         } else {
