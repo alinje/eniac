91071b5d5c8b3d7c28697bd39cf633a9c01ef992
Aline <aline.eikeland@gmail.com>
2021-04-21 18:27:17 +0200

    Implementation of a line graph

--- project/client/pages/labeledStats.js
+++ project/client/pages/labeledStats.js
@@ -1,14 +1,34 @@
 import Link from 'next/link'
+import styles from '../styles/Home.module.css' // TODO a common CSS strategy is needed
+import Head from 'next/head' 
+
+import LineDiagram from '../public/LineDiagram.js'
 
 export default function LabeledStats() {
     return (
-        <>
-            <h1>First Page</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
+        /* TODO something about flex is messing with the style classes */
+        <div /*className={styles.container}*/>
+            <Head>
+                <title>Stocks sorted by labels</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main /*className={styles.main}*/>
+                {/* Link to homepage*/}
+                <div className={styles.grid}>
+                    <Link href="/" passHref>
+                        <div className={styles.card}>
+                            Back to homepage
+                        </div>        
+                    </Link>
+
+                    {/* Visualization over all stocks in a label catagory*/}
+                    <div className={styles.card} >
+                        <LineDiagram width={1000}/>
+                    </div>
+                </div>
+
+
+            </main>
+        </div>
     )
 }
