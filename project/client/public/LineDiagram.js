971feb37872d90e6796448a4dae93100c3a070ed
Aline <aline.eikeland@gmail.com>
2021-04-22 12:57:02 +0200

    LineDiagram: Added background grid, moved styling to CSS, add. styling

--- project/client/public/LineDiagram.js
+++ project/client/public/LineDiagram.js
@@ -32,68 +32,101 @@ export default class LineDiagram extends Component {
         //d3.json(props.label, () => fetch().then((res) => res.json))
         //d3.json('sample.json', 
 
-        const data = {
+
+        // no spaces allowed as graph names. Hyphens are replaced with spaces
+        var data = {
             y: "Type of y",
-            series: [{ name: "graph1", values: [10, 30, 70, 90, 100] }, { name: "graph2", values: [5, 26, 80, 70, 60] }],
+            series: [{ name: "graph-1", values: [10, 30, 70, 90, 100] }, { name: "graph-2", values: [5, 26, 80, 70, 60] }, { name: "graph-3", values: [3, 37, 82, 77, 66] }],
             dates: [12, 13, 14, 15, 16]
         }
 
 
-        const margin = ({ top: 20, right: 30, bottom: 30, left: 40 })
+        var margin = ({ top: 20, right: 30, bottom: 30, left: 40 })
 
-        const height = 1000
+        var height = 1000
         var width = 1000
         if (typeof this.props.width != 'undefined') {
             width = this.props.width;
         }
+        if (typeof this.props.height != 'undefined') {
+            height = this.props.height;
+        }
 
 
-        const x = d3.scaleUtc()
+        var x = d3.scaleUtc()
             .domain(d3.extent(data.dates))
             //.domain(d3.extent(data.dates , d => d.date))
             .range([margin.left + 30, width - margin.right])
 
 
+
         //var maxValue = d3.max(data, d => d.value)
         var maxValue = d3.max(data.series, d => d3.max(d.values))
-        const y = d3.scaleLinear()
+        var y = d3.scaleLinear()
             // this is from zero to max. should be able to choose settings for y-axim
             .domain([0, maxValue + maxValue / 10]).nice()
             .range([height - margin.bottom, margin.top])
 
-        // the drawing of the axises
-        const xAxis = g => g
+
+        // functions for the drawing of the axises
+        var xAxis = g => g
             .attr("transform", `translate(0,${height - margin.bottom})`)
             .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+            .attr("class", "axisLabel")
 
-        const yAxis = g => g
-            .attr("transform", `translate(${margin.left},0)`)
+        var yAxis = g => g
+            .attr("transform", `translate(${margin.left},0)`) // not as css since dependent on js
             .call(d3.axisLeft(y))
+            .attr("class", "axisLabel")
             .call(g => g.select(".domain").remove())
             .call(g => g.select(".tick:last-of-type text").clone()
-                .attr("x", 5) //x-position of label shown at top of y-axis
-                .attr("text-anchor", "start")
-                .attr("font-weight", "bold")
+                .attr("class", "yAxisLabel")
+                .attr("x", 10) //x-position of label shown at top of y-axis
                 .text(data.y)) // label shown at top of y-axis
 
+        // creates a background grid
+        var grid = g => g
+            .attr("stroke", "currentColor")
+            .attr("stroke-opacity", 0.1)
+            .call(g => g.append("g")
+                .selectAll("line")
+                .data(x.ticks())
+                .join("line")
+                .attr("x1", d => 0.5 + x(d))
+                .attr("x2", d => 0.5 + x(d))
+                .attr("y1", margin.top)
+                .attr("y2", height - margin.bottom))
+            .call(g => g.append("g")
+                .selectAll("line")
+                .data(y.ticks())
+                .join("line")
+                .attr("y1", d => 0.5 + y(d))
+                .attr("y2", d => 0.5 + y(d))
+                .attr("x1", margin.left)
+                .attr("x2", width - margin.right));
+
         // a line, defined by x-value from every node in data´s date value, and x-value from its value-value
-        const line = d3.line()
+        var line = d3.line()
             .defined(d => !isNaN(d))
             .x((d, i) => x(data.dates[i]))
             .y(d => y(d))
 
 
         // Make an svg and set size of viewPort
-        const svg = d3.select(this.myRef.current)
+        var svg = d3.select(this.myRef.current)
             .append("svg")
             .attr("viewBox", [0, 0, width, height])
             .style("preserveAspectRatio", "xMidYMid meet");
 
+        // append the axises and the grid
         svg.append("g")
             .call(xAxis);
 
         svg.append("g")
             .call(yAxis);
+
+        svg.append("g")
+            .call(grid);
 
         // pretty colors!!
         svg.append("linearGradient")
@@ -102,7 +135,7 @@ export default class LineDiagram extends Component {
             // below: no idea why this is what works
             .attr("x1", 0)
             .attr("y1", 0)
-            .attr("x2", 0)
+            .attr("x2", "20%")
             .attr("y2", "100%")
             .selectAll("stop")
             .data([
@@ -124,29 +157,40 @@ export default class LineDiagram extends Component {
             .data(data.series)
             .enter()
             .append("path")
-            .attr("class", d => d.name )
+            .attr("class", d => d.name)
             .join("path")
             .style("mix-blend-mode", "multiply") // darker where the lines cross over each other
             .attr("d", d => line(d.values));
 
-        svg.selectAll("myLegend")
+        // the creation of the interactive legend
+        svg.selectAll("legend")
             .data(data.series)
             .enter()
             .append('g')
             .append("text")
-            .attr('x', function (d, i) { return 100 + i * 100 })
+            .attr("class", "legend")
+            .attr('x', function (d, i) { return 140 + i * 100 })
             .attr('y', 30)
-            .text(d => d.name)
-            .style("fill", "url(#line-gradient)"/*function (d) { return myColor(d.name) }*/)
-            .style("font-size", 25)
+            .text(d => d.name.replace('-', ' ')) // these are replaced back and forth. This is since class names can not contain spaces
+            .style("fill", "url(#line-gradient)")
             .on("click", d => {
-                let className = "." + d.target.innerHTML // this is dumb
+                let className = "." + d.target.innerHTML.replace(' ', '-') // this is dumb
                 // is the element currently visible ?
                 var currentOpacity = d3.selectAll(className).style("opacity")
                 // Change the opacity: from 0 to 1 or from 1 to 0
                 d3.selectAll(className).transition().style("opacity", currentOpacity == 1.0 ? 0.0 : 1.0)
-
             })
+
+        // tooltips
+        var tooltip = d3.selectAll()
+
+        var updateChart = () => {
+
+        }
+
+        var brush = d3.brushX()
+            .extent([[0, 0], [width, height]])
+            .on("end", updateChart)
     }
 
 
