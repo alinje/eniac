993d0ea4e11ce93d384db35869ee874bcc90dd56
Aline <aline.eikeland@gmail.com>
2021-04-23 14:24:50 +0200

    Addition of two test functions in app.js

--- project/server/app.js
+++ project/server/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') // node´s own import system
 const cors = require('cors')
 const app = express()
 const port = 3001
@@ -6,7 +6,7 @@ const port = 3001
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get("/home", (req, res) => {
@@ -15,6 +15,31 @@ app.get("/home", (req, res) => {
     })
 })
 
+app.get("/singleStockGrowth", (res, req) => {
+    res.send({
+        tesla: [
+            10, 12, 15, 16, 14, 17, 19
+        ]
+    })
+})
+
+app.get("/label"), (res, req) => {
+    res.send({
+        coronaWinners: [{
+            "stock": "Tesla",
+            "labels": [
+                "kort", "coronavinnare"
+            ]
+        },
+        {
+            "stock": "Snapchat",
+            "labels": [
+                "coronavinnare"
+            ]
+        }]
+    })
+}
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
