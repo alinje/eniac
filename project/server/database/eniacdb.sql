4bc49b53122fc1f495aa83c4695f29c9ead4726d
Emil <emil@havsvik.nu>
2021-05-03 11:31:39 +0200

    Some touch ups

--- project/server/database/eniacdb.sql
+++ project/server/database/eniacdb.sql
@@ -78,6 +78,7 @@ INSERT INTO Portfolios VALUES('Alex','Scania',654);
 INSERT INTO Portfolios VALUES('Albin','Volvo',43);
 INSERT INTO Portfolios VALUES('Albin','Scania',654);
 INSERT INTO Portfolios VALUES('Albin','Einride',345);
+INSERT INTO Portfolios VALUES('Alex','Einride',345);
 INSERT INTO Portfolios VALUES('Carl','Einride',1234);
 INSERT INTO Portfolios VALUES('Carl','Volvo',123);
 INSERT INTO Portfolios VALUES('Emil','Snapchat',123);
