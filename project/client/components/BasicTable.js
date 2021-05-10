5f293879a8603d591f9d017e94624f23d759c817
Aline <aline.eikeland@gmail.com>
2021-05-10 22:01:10 +0200

    Removed dead code from BasicTable.js

--- project/client/components/BasicTable.js
+++ project/client/components/BasicTable.js
@@ -86,17 +86,6 @@ export default function BasicTable(props) {
     }
 
 
-    function filterGreaterThan(rows, id, filterValue) {
-        return rows.filter((row) => {
-            const rowValue = row.values[id];
-            return rowValue >= filterValue;
-        });
-    }
-
-    filterGreaterThan.autoRemove = (val) => typeof val !== "number";
-
-
-
     function SliderColumnFilter({
         column: { filterValue = [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER], preFilteredRows, setFilter, id },
     }) {
@@ -135,61 +124,7 @@ export default function BasicTable(props) {
         )
     }
 
-    function NumberRangeColumnFilter({
-        column: { filterValue = [], preFilteredRows, setFilter, id }
-    }) {
-        const [min, max] = React.useMemo(() => {
-            let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-            let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-            preFilteredRows.forEach((row) => {
-                min = Math.min(row.values[id], min);
-                max = Math.max(row.values[id], max);
-            });
-            return [min, max];
-        }, [id, preFilteredRows]);
 
-        return (
-            <div
-                style={{
-                    display: "flex"
-                }}
-            >
-                <input
-                    value={filterValue[0] || ""}
-                    type="number"
-                    onChange={(e) => {
-                        const val = e.target.value;
-                        setFilter((old = []) => [
-                            val ? parseInt(val, 10) : undefined,
-                            old[1]
-                        ]);
-                    }}
-                    placeholder={`Min (${min})`}
-                    style={{
-                        width: "70px",
-                        marginRight: "0.5rem"
-                    }}
-                />
-            to
-                <input
-                    value={filterValue[1] || ""}
-                    type="number"
-                    onChange={(e) => {
-                        const val = e.target.value;
-                        setFilter((old = []) => [
-                            old[0],
-                            val ? parseInt(val, 10) : undefined
-                        ]);
-                    }}
-                    placeholder={`Max (${max})`}
-                    style={{
-                        width: "70px",
-                        marginLeft: "0.5rem"
-                    }}
-                />
-            </div>
-        );
-    }
 
     function fuzzyTextFilterFn(rows, id, filterValue) {
         return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
