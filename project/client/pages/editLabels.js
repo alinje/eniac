802b07b5d1ad8e0fe898d552b606f30b32f4fd45
Emil <emil@havsvik.nu>
2021-05-12 17:19:18 +0200

    Added a working autocomplete form for deleting labels, and a not working one for stocks...

--- project/client/pages/editLabels.js
+++ project/client/pages/editLabels.js
@@ -13,13 +13,19 @@ import {
 import LineDiagram from '../public/LineDiagram.js'
 import DistributionChart from '../public/DistributionChart.js'
 import BasicTable from "../components/BasicTable";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
 
 
 export default function EditLabels() {
 	const queryClient = useQueryClient()
 	const {data} = useQuery("dbConnect", () => fetch("http://localhost:3001/get-labels").then(((res) => res.json()))) // despite the name, does not return a JSON object
-	const {data2} = useQuery("dbConnect", () => fetch("http://localhost:3001/get-stocks-with-labels").then(((res) => res.json()))) // despite the name, does not return a JSON object
-    
+	//const {data2} = useQuery("dbConnect", () => fetch("http://localhost:3001/get-stocks-with-labels").then(((res) => res.json()))) // despite the name, does not return a JSON object
+
+    //Variables that becomes the current selected item
+    let deleteLabelSelected = null
+    let stockSelected = null
 	
     //Sends the added label name through JSON to the server
 	const addLabel = async event => {
@@ -36,14 +42,16 @@ export default function EditLabels() {
 	 //Sends the deleted label name through JSON to the server
 	const deleteLabel = async event => {
         event.preventDefault()
-        const res = await fetch('http://localhost:3001/deleteLabel', {
+        if(deleteLabelSelected != null){
+            const res = await fetch('http://localhost:3001/deleteLabel', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({label: event.target.name.value})
-        },window.location.reload())
-        const result = await res.json()
+                body: JSON.stringify({label: deleteLabelSelected.name})
+            }, window.location.reload())
+            const result = await res.json()
+	    }
 	}
-	
+
 	//Sends the stock, label and weight through JSON to the server
 	const addLabelToStock= async event => {
         event.preventDefault()
@@ -90,7 +98,28 @@ export default function EditLabels() {
         },window.location.reload())
 		const result = await res.json()
     }
-	//window.location.reload();
+
+    //Constant containing the labels
+    const labelProps = {
+        options: data,
+        getOptionLabel: (option) => option.name,
+    };
+    //Constant that SHOULD contain the stocks! But contains the labels for now....
+    const stockProps = {
+        options: data,
+        getOptionLabel: (option) => option.name,
+    };
+
+    // const options = data.map((option) => {
+    //     const firstLetter = option.name[0].toUpperCase();
+    //     return {
+    //         firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+    //         ...option,
+    //     };
+    // });
+
+
+
     return (
         /* TODO something about flex is messing with the style classes */
         <div /*className={styles.container}*/>
@@ -99,16 +128,17 @@ export default function EditLabels() {
                 <link rel="icon" href="/favicon.ico" />
 
             </Head>
-            <main /*className={styles.main}*/>
+            <main  /*className={styles.main}*/>
                 {/* Link to homepage*/
                 /* Forms for editing and adding labels*/
                 }
     
 				    <div className={styles.fillLeft}>
-                
                     <div>
+
                     <h1>Edit labels</h1>
-					    {/* A form for adding a label*/}
+
+                {/* A form for adding a label*/}
                 <h2>Add new label</h2>
                 <form onSubmit={addLabel}>
                     <input id="name" name="name" type="text" placeholder="Label" autoComplete="name" required />
@@ -119,12 +149,6 @@ export default function EditLabels() {
                 <h2>Delete label</h2>
                 <form onSubmit={deleteLabel}>
 				    <input id="name" name="name" type="text" placeholder="Label" autoComplete="name" required />
-				    <select>
-						<option value={data}></option>
-                        <option value="lime">Lime</option>
-                        <option value="coconut">Coconut</option>
-                        <option value="mango">Mango</option>
-                    </select>
                     <button type="submit">Delete label</button>
                 </form>
 
@@ -159,11 +183,45 @@ export default function EditLabels() {
                     
 				<div className={styles.fillLeft}>
                 <p><BasicTable dataRows={data} /></p>
-                <p><BasicTable dataRows={data2} /></p>
                 </div>
-					
                 </div>
-                
+
+                <div style={{ width: 300 }}>
+                {/* New form for deleting a label*/}
+                <Autocomplete
+                    onChange={(event, value) => deleteLabelSelected = value}
+                    {...labelProps}
+                    id="Delete Label"
+                    clearOnEscape
+                    renderInput={(params) => <TextField {...params} label="Delete Label" margin="normal" />}
+                />
+                <Button variant="contained" color="secondary" onClick={deleteLabel}>
+                    Delete Label
+                </Button>
+
+                {/* New form for stocks (currently shows labels)*/}
+                <Autocomplete
+                    onChange={(event, value) => stockSelected = value}
+                    {...stockProps}
+                    id="Stock"
+                    clearOnEscape
+                    renderInput={(params) => <TextField {...params} label="Stock" margin="normal" />}
+                />
+                <Button variant="contained" color="secondary">
+                    Stock
+                </Button>
+
+                {/*<Autocomplete*/}
+                {/*    id="grouped-demo"*/}
+                {/*    options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}*/}
+                {/*    groupBy={(option) => option.firstLetter}*/}
+                {/*    getOptionLabel={(option) => option.name}*/}
+                {/*    style={{ width: 300 }}*/}
+                {/*    renderInput={(params) => <TextField {...params} label="With categories" variant="outlined" />}*/}
+                {/*/>*/}
+
+                </div>
+
 
                 <div className={styles.grid}>
                     <Link href="/" passHref>
@@ -173,8 +231,10 @@ export default function EditLabels() {
                     </Link>
                 </div>
 
-    
+
             </main>
+
+
         </div>
     )
 }
